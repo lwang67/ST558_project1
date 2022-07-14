f52002d7dbc187bd20734aebe87cbf4c052375cf
Li Wang <106117423+lwang67@users.noreply.github.com>
2022-07-14 17:42:05 -0600

    Create RenderCode.R

---
+++ RenderCode.R
@@ -0,0 +1,5 @@
+rmarkdown::render("Project1RMarkdown.Rmd", 
+                  output_format = "github_document",
+                  output_file = "README.md",
+                  output_options = list(html_preview= FALSE,toc=TRUE,toc_depth=2,toc_float=TRUE)
+)
