5422d385244560d7a993d080022d64a9977a0bd0
Li Wang <106117423+lwang67@users.noreply.github.com>
2022-07-14 23:54:42 -0600

    Update RenderCode.R

--- RenderCode.R
+++ RenderCode.R
@@ -1,4 +1,4 @@
-rmarkdown::render("Project1.Rmd", 
+rmarkdown::render("project1.Rmd", 
                   output_format = "github_document",
                   output_file = "README.md",
                   output_options = list(html_preview= FALSE,toc=TRUE,toc_depth=2,toc_float=TRUE)
